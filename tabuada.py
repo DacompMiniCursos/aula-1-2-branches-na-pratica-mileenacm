84614ef06356ed76866c76012312211676da492e
mileenacm <vibesstudy2@gmail.com>
2026-05-17 14:47:21 -0300

    tabuada com while

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+def tabuada():
+
+    numero = int(input("Digite o número: "))
+
+    i = 1
+    while i <= 10:
+        print(f"{numero} x {i} = {numero*i}")
+        i += 1
+    return
+
+tabuada()
